acd8f15f06febad1534ccef3665a74b38beb774d
chenjiandongx <chenjiandongx@qq.com>
2018-03-30 23:31:41 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,11 @@
+# rlp
+> range like python
+
+模仿 Python range 函数
+
+
+## 示例
+``` javascript
+```
+
+## LICENSE [MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,34 @@
+module.exports = function*(start, stop, step = 1) {
+
+    let args = [...arguments];
+
+    if (args.length === 0) {
+        return
+    } else if (args.length === 1) {
+        start = 0;
+        [stop] = args
+    } else if (args.length === 2) {
+        [start, stop] = args
+    } else if (args.length === 3) {
+        [start, stop, step] = args
+    }
+
+    function isInteger(...num) {
+        for (let n of num) {
+            if (!(typeof n === 'number') || !(num % 1 === 0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    if (!(isInteger(start, stop, step))) {
+        return null
+    }
+    if (step < 0 || stop < start) {
+        return null;
+    }
+    for (let i = start; i < stop; i += step) {
+        yield i
+    }
+}
